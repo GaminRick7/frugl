d05a739a24c855fb1d3ac21f97f5986389f076a0
Parsa Nabifar <parsanabifar1385@gmail.com>
2025-11-30 11:27:04 -0500

    Updated presenter

--- src/main/java/interface_adapter/import_statement/ImportStatementPresenter.java
+++ src/main/java/interface_adapter/import_statement/ImportStatementPresenter.java
@@ -1,11 +1,11 @@
 package interface_adapter.import_statement;
 
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+
 import interface_adapter.ViewManagerModel;
 import use_case.import_statement.ImportStatementOutputBoundary;
 import use_case.import_statement.ImportStatementOutputData;
-
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 
 /**
  * The Presenter for the Import Bank Statement Use Case.
@@ -15,7 +15,8 @@ public class ImportStatementPresenter implements ImportStatementOutputBoundary {
     private final ViewManagerModel viewManagerModel;
     private final ImportStatementViewModel importStatementViewModel;
 
-    public ImportStatementPresenter(ViewManagerModel viewManagerModel, ImportStatementViewModel importStatementViewModel) {
+    public ImportStatementPresenter(ViewManagerModel viewManagerModel,
+                                    ImportStatementViewModel importStatementViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.importStatementViewModel = importStatementViewModel;
     }
@@ -23,9 +24,9 @@ public class ImportStatementPresenter implements ImportStatementOutputBoundary {
     @Override
     public void prepareSuccessView(ImportStatementOutputData outputData) {
 
-        YearMonth month = outputData.getStatementMonth();
+        final YearMonth month = outputData.getStatementMonth();
 
-        String message = "Importing Successful for " + month.format(DateTimeFormatter.ofPattern("MMMM yyyy"));
+        final String message = "Importing Successful for " + month.format(DateTimeFormatter.ofPattern("MMMM yyyy"));
         viewManagerModel.showPopup(message);
         importStatementViewModel.setState("");
         importStatementViewModel.firePropertyChange("filePath");
@@ -33,7 +34,7 @@ public class ImportStatementPresenter implements ImportStatementOutputBoundary {
 
     @Override
     public void prepareFailView(String errorMessage) {
-        String message = "Importing Unsuccessful: " + errorMessage;
+        final String message = "Importing Unsuccessful: " + errorMessage;
         viewManagerModel.showPopup(message);
         importStatementViewModel.setState("");
         importStatementViewModel.firePropertyChange("filePath");
