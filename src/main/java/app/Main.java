41b45e2608798fbb1e78c66cd9c80e774727a31f
ksrhhh <105606697+ksrhhh@users.noreply.github.com>
2025-12-01 12:24:26 -0500

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,7 +17,7 @@ public class Main {
      * @param args command line arguments (not used)
      * @throws Exception if an error occurs during application initialization
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
                 .addDashboardView()
@@ -33,18 +33,17 @@ public class Main {
                 .build();
 
         application.setExtendedState(Frame.MAXIMIZED_BOTH);
-        final SetGoalController goalController = appBuilder.getSetGoalController();
-        if (goalController != null) {
-            goalController.loadForest();
-        }
-
-        application.pack();
         application.setLocationRelativeTo(null);
         application.setVisible(true);
 
         final DashboardView dashboardView = appBuilder.getDashboardView();
         if (dashboardView != null) {
             dashboardView.loadInitialData();
+        }
+
+        final SetGoalController goalController = appBuilder.getSetGoalController();
+        if (goalController != null) {
+            goalController.loadForest();
         }
     }
 }
